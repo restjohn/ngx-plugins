4a241c959093164a7003ad7a8b397861a8faaff8
Robert St. John <restjohn@users.noreply.github.com>
2023-04-11 09:14:56 -0600

    [eg-plugin1][angular upgrade] ng update @angular/core@11 @angular/cli@11

--- eg-plugin1/projects/eg-plugin1/src/lib/eg-plugin1.component.spec.ts
+++ eg-plugin1/projects/eg-plugin1/src/lib/eg-plugin1.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { EgPlugin1Component } from './eg-plugin1.component';
 
@@ -6,7 +6,7 @@ describe('EgPlugin1Component', () => {
   let component: EgPlugin1Component;
   let fixture: ComponentFixture<EgPlugin1Component>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ EgPlugin1Component ]
     })
