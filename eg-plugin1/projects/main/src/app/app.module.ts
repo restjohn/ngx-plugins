1d71f9b65fc47ead6abc69e356a2eae203bfb9b9
Robert St. John <restjohn@users.noreply.github.com>
2023-06-01 20:11:44 -0600

    [eg-plugin1] add plugin module to app imports to ensure typescript compiles it

--- eg-plugin1/projects/main/src/app/app.module.ts
+++ eg-plugin1/projects/main/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EgPlugin1Module } from './eg-plugin1.module'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    EgPlugin1Module,
   ],
   providers: [],
   bootstrap: [AppComponent]
