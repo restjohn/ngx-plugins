5e9203abe912eee3dec701feb50a4e4cdb50d822
Robert St. John <restjohn@users.noreply.github.com>
2024-04-12 23:36:34 -0600

    [eg-core] wip: schematic rule to add the plugin hook to library entry point

--- eg-core/projects/eg-core-lib/schematics/eg-plugin-lib/index.ts
+++ eg-core/projects/eg-core-lib/schematics/eg-plugin-lib/index.ts
@@ -1,12 +1,55 @@
 import { Rule, chain, externalSchematic, SchematicsException,  } from '@angular-devkit/schematics'
 import { Schema as BaseLibraryOptions } from '@schematics/angular/library/schema'
-import {} from '@schematics/angular/utility/paths'
-import { updateWorkspace } from '@schematics/angular/utility/workspace'
-import {} from '@schematics/angular/utility/workspace-models'
+import { getWorkspace, updateWorkspace,  } from '@schematics/angular/utility/workspace'
+import { insertImport, insertAfterLastOccurrence, findNodes } from '@schematics/angular/utility/ast-utils'
+import { applyToUpdateRecorder } from '@schematics/angular/utility/change'
+import * as ts from '@schematics/angular/third_party/github.com/Microsoft/TypeScript/lib/typescript'
+import { JsonObject } from '@angular-devkit/core'
 
-// async function generatePluginHooks(options: EgPluginLibraryOptions, tree: Tree, context: SchematicContext): Promise<Rule> {
-//  throw new Error('todo')
-// }
+function addPluginHookToEntryPoint(options: EgPluginLibraryOptions): Rule {
+  const entryFile = options.entryFile
+  if (!entryFile) {
+    throw new SchematicsException('missing entryFile')
+  }
+  return async (tree, context) => {
+    const workspace = await getWorkspace(tree)
+    const project = workspace.projects.get(options.name)
+    if (!project) {
+      throw new SchematicsException(`project not found in workspace: ${project}`)
+    }
+    const entryFilePath = `${project.sourceRoot}/${entryFile}.ts`
+    const entryFileText = tree.readText(entryFilePath)
+    const entrySource = ts.createSourceFile(entryFilePath, entryFileText, ts.ScriptTarget.Latest, true)
+    const importChange = insertImport(entrySource, entryFilePath, 'EgPlugin', '@ng-plugins/eg-core-lib')
+    const packageJson = tree.readJson(`${project.root}/package.json`) as JsonObject
+    const packageName = packageJson.name as string
+    const pluginExport =
+`
+export const plugin: EgPlugin = {
+  id: '${packageName}',
+  title: '${packageName}',
+  component: null,
+}
+`
+    type ExportNodes = ts.ExportAssignment | ts.ExportDeclaration | ts.ExportSpecifier | ts.NamedExports
+    const exportNodes = findNodes<ExportNodes>(entrySource, ((x: ts.Node) => {
+      return [
+        ts.SyntaxKind.ExportAssignment,
+        ts.SyntaxKind.ExportDeclaration,
+        ts.SyntaxKind.ExportSpecifier,
+        ts.SyntaxKind.NamedExports,
+      ].includes(x.kind)
+    }) as ((node: ts.Node) => node is ExportNodes))
+    .sort((a, b) => a.getStart() - b.getStart())
+    console.log(exportNodes)
+    const pluginExportChange = insertAfterLastOccurrence(exportNodes, pluginExport, entryFilePath, entrySource.end)
+    const edit = tree.beginUpdate(entryFilePath)
+    applyToUpdateRecorder(edit, [ importChange, pluginExportChange ])
+    tree.commitUpdate(edit)
+    console.log(exportNodes)
+    context.logger.info(`--- entry file ${entryFilePath}\n${entryFileText}`)
+  }
+}
 
 function usePluginBuilder(options: EgPluginLibraryOptions): Rule {
   return updateWorkspace(workspace => {
@@ -26,7 +69,7 @@ function usePluginBuilder(options: EgPluginLibraryOptions): Rule {
 
 export function egPluginLib(options: EgPluginLibraryOptions): Rule {
   if (!options.name) {
-    throw new SchematicsException('eg-plugin-lib generator requires a project name')
+    throw new SchematicsException('missing name')
   }
   return chain([
     (_tree, context) => {
@@ -34,6 +77,7 @@ export function egPluginLib(options: EgPluginLibraryOptions): Rule {
     },
     externalSchematic('@schematics/angular', 'library', options),
     usePluginBuilder(options),
+    addPluginHookToEntryPoint(options),
   ])
 }
 
